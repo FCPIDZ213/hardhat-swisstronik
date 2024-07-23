d88f59f9df17921451a62098883b604c4df2c4c2
DZ-staking <112197568+FCPIDZ213@users.noreply.github.com>
2024-07-23 05:48:24 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: ["eceb4f55303829e5a7509b74c39a966afa61ee42b947d60b3431d065154ef4a8"]  // Remplacez par votre clé privée
+      accounts: [""]  // Remplacez par votre clé privée
     }
   }
 };
